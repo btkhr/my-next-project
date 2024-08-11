5bd7af6d2bdfde245e8dc7c403e17d7c6ed37cf9
btkhr <tkhr.bsh@gmail.com>
2024-08-11 15:48:11 +0900

    chap4

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // <html lang="en">
-    //   <body className={inter.className}>{children}</body>
-    // </html>
     <html lang="ja">
       <body>
         <Header />
